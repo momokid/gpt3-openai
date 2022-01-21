dc46d488ebfa9d74565d071e9a0b69b54b4a46ed
anwar <anwar.rhsl@gmail.com>
2022-01-21 09:19:35 +0000

    Initial commit

---
+++ src/containers/whatGPT3/WhatGP3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatgpt3.css'
+
+const WhatGP3 = () => {
+    return (
+        <div>
+            What GP-3
+        </div>
+    )
+}
+
+export default WhatGP3
